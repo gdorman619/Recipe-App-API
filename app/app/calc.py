6167aabd24fbc51107f160479205a4f2df07afc4
garydorman <garydorman619@gmail.com>
2023-12-16 10:45:32 -0500

    Updated files in accordance with PEP8

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,7 +1,9 @@
 """ Calulator functions """
 
-def add(x,y):
+
+def add(x, y):
     return x + y
 
-def subtract(x,y):
+
+def subtract(x, y):
     return x - y
--- app/app/test.py
+++ app/app/test.py
@@ -3,19 +3,20 @@
 from django.test import SimpleTestCase
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
     """ Calc Tests """
 
     def test_add_numbers(self):
-         """ Test adding numbers together """
+        """ Test adding numbers together """
 
-         result = calc.add(1,2)
+        result = calc.add(1, 2)
 
-         self.assertEqual(result, 3)
+        self.assertEqual(result, 3)
 
     def test_subtract_numbers(self):
-         """ Test subtracting numbers together """
+        """ Test subtracting numbers together """
 
-         result = calc.subtract(10,15)
+        result = calc.subtract(10, 15)
 
-         self.assertEqual(result, -5)
+        self.assertEqual(result, -5)
