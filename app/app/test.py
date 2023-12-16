7056f5f88ea4453294ad87e7900f6f2754147f60
garydorman <garydorman619@gmail.com>
2023-12-16 10:32:08 -0500

    Added Postgresql along with depenencies

---
+++ app/app/test.py
@@ -0,0 +1,21 @@
+""" Sample Tests """
+
+from django.test import SimpleTestCase
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    """ Calc Tests """
+
+    def test_add_numbers(self):
+         """ Test adding numbers together """
+
+         result = calc.add(1,2)
+
+         self.assertEqual(result, 3)
+
+    def test_subtract_numbers(self):
+         """ Test subtracting numbers together """
+
+         result = calc.subtract(10,15)
+
+         self.assertEqual(result, -5)
