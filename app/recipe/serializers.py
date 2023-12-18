dbc834d71a690fc8e825a037dc3cf16146ec9b35
Gary Dorman <garydorman619@gmail.com>
2023-12-18 17:44:04 -0500

    Lint formatting

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -4,13 +4,15 @@ from rest_framework import serializers
 
 from core.models import Recipe
 
+
 class RecipeSerializer(serializers.ModelSerializer):
     """Serializers for recipes"""
 
     class Meta:
         model = Recipe
-        fields = ['id','title','time_minutes','price','link']
+        fields = ['id', 'title', 'time_minutes', 'price', 'link']
         read_only_fields = ['id']
+
 
 class RecipeDetailSerializer(RecipeSerializer):
     """Serializer for Recipe Detail view"""
